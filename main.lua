a070a9500651bd70cfa854f6ff985281c94da065
Boris Yakubchik <yboris@yahoo.com>
2020-04-06 19:11:51 -0400

    draw angle and force in UI

--- main.lua
+++ main.lua
@@ -19,6 +19,8 @@ function love.load()
 
     canvas = love.graphics.newCanvas(WIDTH, HEIGHT)
 
+    canvas2 = love.graphics.newCanvas(WIDTH, HEIGHT)
+
     -- love.graphics.setCanvas(canvas)
     --     love.graphics.setBlendMode("alpha") -- what does this do ?!??
     -- love.graphics.setCanvas()
@@ -26,7 +28,7 @@ function love.load()
     newGame()
 
     bulType = 3
-    playerPressedShootButton() -- disable this to let the player take the first shot
+    -- playerPressedShootButton() -- disable this to let the player take the first shot
 
 end
 
@@ -62,15 +64,9 @@ function love.draw()
 
         for i = 1, numOfBullets do collisonCheck(i) end
 
-        -- benign makes your own bullet not kill you for the first few moments when you shoot
-        benign = benign + 1
-    end
+        benign = benign + 1 -- benign makes your own bullet not kill you for the first few moments when you shoot
 
-    if shotInProgress == true then
-
-        -- if the x location of each shot == 0 then end the turn
-        bulletsInFlight = false
-
+        bulletsInFlight = false -- TODO: fix this hacky thing: if the x location of each shot == 0 then end the turn
         for i = 1, numOfBullets do
             if allBullets[i].x ~= 0 and allBullets[i].y ~= 0 then
                 bulletsInFlight = true -- HACK -- assumes position of disabled bullet is = (0, 0)
@@ -81,6 +77,7 @@ function love.draw()
             print("all shots have finished")
 
             shotInProgress = false
+
             if turn == 1 then
                 turn = 2
             elseif turn == 2 then
@@ -91,6 +88,20 @@ function love.draw()
             drawUI()
             love.graphics.setCanvas()
         end
+
+    else
+
+        love.graphics.setCanvas(canvas2)
+        love.graphics.clear()
+
+        if turn == 1 then
+            drawPlayerAngleAndForce(player1)
+        else
+            drawPlayerAngleAndForce(player2)
+        end
+
+        love.graphics.setCanvas()
+        love.graphics.draw(canvas2)
 
     end
 
@@ -109,5 +120,20 @@ function love.draw()
 
     -- RENDER THE CANVAS NOW
     love.graphics.draw(canvas)
+
+end
+
+
+function drawPlayerAngleAndForce(playerN)
+
+    love.graphics.ellipse('line', playerN.x, playerN.y, 10, 10)
+    love.graphics.line(playerN.x, playerN.y,
+                        playerN.x + math.cos(0.0174533 * playerN.angle) * 100,
+                        playerN.y + math.sin(0.0174533 * playerN.angle) * 100)
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.line(playerN.x, playerN.y,
+                        playerN.x + math.cos(0.0174533 * playerN.angle) * 100 * playerN.force / 5,
+                        playerN.y + math.sin(0.0174533 * playerN.angle) * 100 * playerN.force / 5)
+    love.graphics.setColor(1, 1, 1, 1)
 
 end
